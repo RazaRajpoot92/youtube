dfc4df1f089d1169c73d5210c470c9d474740e47
Raza <razarajpoot99598@gmail.com>
2024-05-02 19:01:34 +0500

    Making search functional

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import {useDispatch} from "react-redux"
 import { toggleMenu } from "../utils/appSlice"
 import {Link} from "react-router-dom"
+import { useState } from "react"
 
 const menu = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png"
 const youtubeLogo = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsHa1f7bO91Miz1KKeYpRo95dW2FceKO-yAAW1CzueFQ&s"
@@ -8,13 +9,16 @@ const userIcon = "https://static.vecteezy.com/system/resources/thumbnails/002/31
 const searchIcon = "https://static.vecteezy.com/system/resources/thumbnails/009/652/218/small/magnifying-glass-icon-isolated-on-white-background-search-illustration-vector.jpg"
 
 const Header = ()=>{
+
+    const [searchText, setSearchText] = useState("")
+
     const dispatch = useDispatch()
-    
+
     const handleToggle = ()=>{
         dispatch(toggleMenu())
     }
     return(
-        
+
         <div className="flex top-0 sticky w-full bg-slate-50 shadow-md justify-between p-4 border h-16 items-center">
             <div className="flex gap-1 items-center">
                 <img onClick={()=>handleToggle()} className="h-6 cursor-pointer" src={menu} alt="" />
@@ -23,7 +27,10 @@ const Header = ()=>{
                 </Link>
             </div>
             <div className="flex items-center">
-                <input className="border text-gray-600 border-gray-400 p-1 w-[430px] pl-4 rounded-l-full focus:outline-none" type="text" />
+                <input
+                value={searchText}
+                onChange={(e)=>setSearchText(e.target.value)}
+                className="border text-gray-600 border-gray-400 p-1 w-[430px] pl-4 rounded-l-full focus:outline-none" type="text" />
                 <button className="border border-gray-400 p-1 px-2 rounded-r-full">
                     <img className="h-6" src={searchIcon} alt="" />
                 </button>
@@ -32,7 +39,7 @@ const Header = ()=>{
                 <img className="h-10" src={userIcon} alt="" />
             </div>
         </div>
-        
+
     )
 }
 
